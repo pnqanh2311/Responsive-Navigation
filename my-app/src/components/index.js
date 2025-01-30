6ce80a9e08142c2cf91fc5ee6ec6238d467fb116
pnqanh2311 <annpn2311@gmail.com>
2025-01-30 12:26:29 -0700

    update

--- my-app/src/components/index.js
+++ my-app/src/components/index.js
@@ -1,1 +1,33 @@
 import React, {useState} from "react";
+const Navbar = () => {
+    const[bClass, setbClass] = useState ("burger-bar unclicked")
+    const[mClass, setmClass]= useState("menu hidden")
+    
+    
+     const [isMenuOpen, setIsMenuOpen] = useState(false);
+   // eslint-disable-next-line
+      const updateMenu = ()=> 
+      {  setIsMenuOpen(!isMenuOpen);
+        if(!isMenuOpen) {
+          setbClass("burger-bar clicked")
+          setmClass ("menu visible")
+        }
+      else{
+        setbClass("burger-bar unclicked")
+        setmClass("menu hidden")
+      }
+      return (
+        <div>
+          <nav>
+          <div className="burger-menu"  onClick={updateMenu}>
+          <div className={bClass} >Home</div>
+              <div className={bClass} >Menu</div>
+              <div className={bClass} >Contact</div>
+            </div>
+          </nav>
+          <div className={mClass}></div>
+          </div>
+        )
+        } ;
+    }
+    export default Navbar;
