72f35e51e382706046e8dce61322a1ed6aa16799
pnqanh2311 <annpn2311@gmail.com>
2025-01-30 12:28:14 -0700

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,5 +2,36 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/index';
 
+const App = () => {
+ 
+  return (
+    <div>
+      <div>
+      <Navbar/>
+     
+      <h1>Annie's Skincare</h1>
+      <nav>
+        <ul >
+            <li><a href="App.js">Home</a></li>
+            <li><a href="#menu" >Menu</a>
+                <ul class="submenu">
+                    <li><a href="#bodycare">BodyCare</a></li>
+                    <li><a href="#skincare">Skincare</a></li>
+                    <li><a href="#makeup">Makeup</a></li>
+                </ul>
+            </li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+    </nav>
+      <main>
+        <h2>Welcome to Annie's Skincare</h2>
+        <p>How can I help you today?</p>
+      </main>
+    </div>
+    </div>
+  );
+}
+   
+
 
 export default App;
